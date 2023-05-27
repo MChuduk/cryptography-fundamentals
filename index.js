41032b6edb13c0c12a7523c5f1b806edcd955e13
MChuduk <mihail2014mihail@gmail.com>
2023-05-27 22:39:24 +0300

    added caesar cipher

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 import http from 'node:http';
+import url from 'node:url';
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
+function caesarCipher(message, key) {
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    const result = [];
+    for (let i = 0; i < message.length; i++) {
+        const char = message[i];
+        const charCode = alphabet.indexOf(char);
+        if (charCode < 0) {
+            result.push(char);
+            continue;
+        }
+        const encryptedCharCode = (charCode + key) % alphabet.length;
+        const encryptedChar = alphabet[encryptedCharCode];
+        result.push(encryptedChar);
+    }
+    return result.join('');
+}
+
 const server = http.createServer((req, res) => {
-    res.end("hello");
+    const { query } = url.parse(req.url, true);
+    const { message = '' } = query;
+    res.end(caesarCipher(message, 5));
 });
 
 server.listen(port, hostname, () => {
